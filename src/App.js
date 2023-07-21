6936947cf3107d87b551e8d8560535b271740cd6
Jnaneswar <nanimca000@gmail.com>
2023-07-21 11:28:52 +0530

    Routing basic structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./Pages/Home";
+import ForgotPassword from "./Pages/ForgotPassword";
+import Offers from "./Pages/Offers";
+import Profile from "./Pages/Profile";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-2xl text-red-400 font-bold">
-        Hello world!
-      </h1>
+    <div className="text-center text-2xl text-red-400 font-bold">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/offers" element={<Offers />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
